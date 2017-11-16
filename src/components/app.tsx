7987c3670ec78b05e6e32632cb9dd939837bc83d
Doug Martin <doug@zoopdoop.com>
2017-11-16 07:53:45 -0500

    Working crud without window display

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,88 @@
 import * as React from "react"
 import * as firebase from "firebase"
+import * as queryString from "query-string"
+import { DocumentCrudComponent } from "./document-crud"
+import { WorkspaceComponent } from "./workspace"
 
-export interface AppProps {}
-export interface AppState {
+export interface AppComponentProps {}
+
+export interface AppComponentState {
   authUser: firebase.User|null
-  authError: firebase.auth.Error|null
+  authError: string|null
+  documentError: string|null
+  documentId: string|null
+  documentRef: firebase.database.Reference|null
+  document: Document|null
 }
 
-export class App extends React.Component<AppProps, AppState> {
-  constructor (props:AppProps) {
+export interface AppParams {
+  document: string|null
+}
+
+export interface WindowProps {
+  top: number
+  left: number
+  width: number
+  height: number
+  url: string
+  title: string
+}
+
+export interface WindowPropsMap {
+  [key: string]: WindowProps
+}
+
+export interface WindowDataMap {
+  [key: string]: any
+}
+
+export interface DocumentData {
+  windows: {
+    props: WindowPropsMap
+    order: string[]
+    data: WindowDataMap
+  }
+}
+
+export interface DocumentInfo {
+  version: "1.0.0",
+  ownerId: string
+  createdAt: number
+  name: string
+}
+
+export interface Document {
+  info: DocumentInfo
+  data?: DocumentData
+}
+
+export function getDocumentRef(fullDocumentId:string) {
+  const [ownerId, documentId, ...rest] = fullDocumentId.split(":")
+  if (!ownerId && !documentId) {
+    return null
+  }
+  if (ownerId === "instance") {
+    return firebase.database().ref(`instances/${documentId}`)
+  }
+  return firebase.database().ref(`templates/${ownerId}/${documentId}`)
+}
+
+export class AppComponent extends React.Component<AppComponentProps, AppComponentState> {
+  startingTitle: string
+
+  constructor (props:AppComponentProps) {
     super(props)
     this.state = {
+      authError: null,
+      documentError: null,
       authUser: null,
-      authError: null
+      documentId: null,
+      documentRef: null,
+      document: null
     }
+    this.startingTitle = document.title
+
+    this.handleDocumentChange = this.handleDocumentChange.bind(this)
   }
 
   componentWillMount() {
@@ -26,35 +95,84 @@ export class App extends React.Component<AppProps, AppState> {
       messagingSenderId: "987825465426"
     })
 
+    let authed = false
     firebase.auth().onAuthStateChanged((authUser) => {
       this.setState({authUser})
+
+      // parse the hash after authenticating the first time
+      if (!authed) {
+        authed = true
+        this.parseHash()
+        window.addEventListener("hashchange", this.parseHash.bind(this))
+      }
     })
 
     firebase.auth().signInAnonymously().catch((authError) => {
-      this.setState({authError})
+      this.setState({authError: authError.toString()})
     })
   }
 
-  renderFirebaseInfo() {
-    const {authError, authUser} = this.state
-    if (authError) {
-      return <div className="firebase-error">{authError.message}</div>
+  componentWillUpdate(nextProps:AppComponentProps, nextState:AppComponentState) {
+    const suffix = nextState.document ? `: ${nextState.document.info.name}` : ""
+    document.title = this.startingTitle + suffix
+  }
+
+  parseHash() {
+    const params:AppParams = queryString.parse(window.location.hash)
+    this.setState({documentId: params.document || null, document: null, documentError: null})
+
+    if (this.state.documentRef) {
+      this.state.documentRef.off("value", this.handleDocumentChange)
     }
-    if (authUser) {
-      if (authUser.isAnonymous) {
-        return <div>You are logged in as an anonymous user in Firebase</div>
+    if (params.document) {
+      const documentRef = getDocumentRef(params.document)
+      if (documentRef) {
+        documentRef.on("value", this.handleDocumentChange)
+        this.setState({documentRef})
       }
-      return <div>{authUser.displayName} ({authUser.email})</div>
+      else {
+        this.setState({documentError: "Invalid collaborative space document in url!"})
+      }
     }
-    return <div>Not logged into Firebase...</div>
+  }
+
+  handleDocumentChange(snapshot:firebase.database.DataSnapshot|null) {
+    const document = (snapshot && snapshot.val()) || null
+    this.setState({document})
+    if (!document) {
+      this.setState({documentError: "Unable to load collaborative space document!"})
+    }
+  }
+
+  renderFatalError(message:string) {
+    return <div className="error">{message}</div>
+  }
+
+  renderProgress(message:string) {
+    return <div className="progress">{message}</div>
   }
 
   render() {
-    return <div>
-      <div className="hello">Hello, world from Travis!</div>
-      <div className="firebase-info">
-        {this.renderFirebaseInfo()}
-      </div>
-      </div>
+    const error = this.state.authError || this.state.documentError
+    if (error) {
+      return this.renderFatalError(error)
+    }
+
+    if (this.state.authUser) {
+      if (this.state.documentId) {
+        if (this.state.documentRef && this.state.document) {
+          return <WorkspaceComponent
+                    authUser={this.state.authUser}
+                    document={this.state.document}
+                    documentRef={this.state.documentRef}
+                    readonly={this.state.authUser.uid !== this.state.document.info.ownerId}
+                 />
+        }
+        return this.renderProgress("Loading collaborative space document...")
+      }
+      return <DocumentCrudComponent authUser={this.state.authUser} />
+    }
+
+    return this.renderProgress("Authenticating...")
   }
 }
